648f33f03aed126d312451657bbc8557d4c0790b
vkkishore <31710285+vkkishore@users.noreply.github.com>
2017-10-12 22:33:04 -0400

    Update spark_analysis.py

--- Week 4/spark_analysis.py
+++ Week 4/spark_analysis.py
@@ -1,6 +1,20 @@
 from pyspark import SparkContext
 from pyspark import SQLContext
+from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.functions import *
+from pyspark.sql.types import StringType
+
 sc = SparkContext()
 sqlContext = SQLContext(sc)
-data = sqlContext.read.csv("/user/admin/week4-hw/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.csv")
-data.write.format("csv").save("/user/ubuntu/sparkingest/raw/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.csv")
+df1 = sqlContext.read.csv("/user/admin/week4-hw/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.csv")
+#df1.write.format("csv").save("/user/ubuntu/sparkingest/raw/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.csv")
+df2 = sqlContext.read.csv("/user/ubuntu/sparkingest/raw/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.csv")
+df2.registerTempTable("Budget")
+df3=sqlContext.sql('select * from Budget where _c0 <> "FUND TYPE"')
+# Create an user defined function to replace ',' for '.'
+#udf = UserDefinedFunction(lambda x: x.replace("$",""), StringType())
+udf = UserDefinedFunction(lambda x: x.replace("$",""), StringType())
+
+df4 = df3.withColumn("_c20", udf(col("_c20")))
+df5 = df4.withColumn("_c21", udf(col("_c21")))
+df5.write.format("orc").save("/user/ubuntu/sparkingest/stage/Budget_-_2017_Budget_Recommendations_-_Positions_and_Salaries.orc")
